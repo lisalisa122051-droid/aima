829bbebe351a48266e9812f675d36094340a3334
lisalisa122051-droid <lisalisa122051@gmail.com>
2025-12-28 14:06:35 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    config: {
+        prefix: ['.', '!'],
+        owner: ['6281234567890'],
+        businessMessage: "Bisnis ini menggunakan layanan aman dari perusahaan Meta untuk mengelola chat ini. Ketuk untuk info selengkapnya.",
+        menuThumbnail: "https://i.imgur.com/9i1GZ1m.jpg",
+        runtime: Date.now(),
+        databaseFile: './database.json'
+    }
+};
